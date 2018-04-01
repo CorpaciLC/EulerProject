509b2e7864d795d0e68a7940418074efec2007c4
Corpaci Luiza <corpaci.luiza@yahoo.com>
2018-04-01 19:15:20 +0300

    Special Pythagorean triplet
    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
    
    a^2 + b^2 = c^2
    For example, 32 + 42 = 9 + 16 = 25 = 52.
    
    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
    Find the product abc.

---
+++ PythagoreanTriplet.java
@@ -0,0 +1,27 @@
+
+public class PythagoreanTriplet extends MultipleOf3And5 {
+	public int a, b, c;
+	public boolean findTriplet(int sum) {
+		int c;
+		for(int i = 1; i < sum; i++) {
+			for(int j = i + 1; j < sum; j++) {
+				c = i*i + j*j;
+				if(this.checkSquare(c) == true) {
+					if( i + j + Math.sqrt(c) ==  1000) {
+						this.a = i;
+						this.b = j;
+						this.c = (int)Math.sqrt(c);
+						System.out.println(i + "^2 + " + j + " ^2 = " + (int)Math.sqrt(c) + "^2");
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	public boolean checkSquare(int n) {
+		if((int)Math.sqrt(n) ==  Math.sqrt(n) )
+			return true;
+		return false;
+	}
+}
